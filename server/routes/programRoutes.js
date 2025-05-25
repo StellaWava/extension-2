e76009eb397f1c657522af4fec1fb67d56848490
StellaWava <stellanaks16@gmail.com>
2025-05-25 00:30:06 -0400

    update all

--- server/routes/programRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-router.get('/', (req, res) => {
-    res.json({ message: 'Programs endpoint working!' });
-});
-export default router;
-//# sourceMappingURL=programRoutes.js.map
--- server/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import programRoutes from './routes/programRoutes';
-const app = express();
-app.use(express.json()); // Automatically parse JSON in request body
-app.use('/api/programs', programRoutes); // Mount routes
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-});
-//# sourceMappingURL=server.js.map
